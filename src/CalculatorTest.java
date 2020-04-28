595f865ac40de392442685251b71b5610cde9c6f
Jeongwon Kim <55073876+gardenk11181@users.noreply.github.com>
2020-04-28 23:34:18 +0900

    Unary must be fixed

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -198,6 +198,17 @@ public class CalculatorTest
 		else return str.substring(count)+" "+result.trim()+" ";
 	}
 
+	public static String[] split(String str) {
+		// rightAsso split
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i)==' ') {
+				System.out.println(str.substring(0,i+1)+", "+str.substring(i+1));
+				return new String[]{str.substring(0,i+1),str.substring(i+1)};
+			}
+		}
+		return new String[]{str,""};
+	}
+
 	public static String[] calculate(Vector<String> input) {
 //		if(isInfix(input) == false) throw Exception();
 
